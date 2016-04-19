85ca2bcf093222c310b4c19b3cce2a2a52676c18
yosufali <yosufali@users.noreply.github.com>
2016-04-19 12:12:17 +0100

    Changed db from sqlite3 to mysql

--- theproject/wbproject/wbproject/settings.py
+++ theproject/wbproject/wbproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'wbp_app',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'wbproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wbpdb',
+        'USER': 'admin',
+        'PASSWORD': 'pass',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
